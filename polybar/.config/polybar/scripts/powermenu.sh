3f12bfae0295880329e3467b9419d1800c3d7ca2
sco <sco>
2024-08-07 20:17:18 +0000

    feat: polybar / change theme + add powermenu

---
+++ polybar/.config/polybar/scripts/powermenu.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+dir="~/.config/polybar/rofi"
+uptime=$(uptime -p | sed -e 's/up //g')
+
+rofi_command="rofi -no-config -theme $dir/powermenu"
+
+lock="󰌾  Lock"
+shutdown="󰐥  Shutdown"
+reboot="󰜉  Restart"
+suspend="󰒲  Sleep"
+logout="󰍃  Logout"
+
+options="$lock\n$suspend\n$logout\n$reboot\n$shutdown\n"
+
+chosen="$(echo -e "$options" | $rofi_command -p "Uptime: $uptime" -dmenu -selected-row 0)"
+case $chosen in
+$shutdown)
+  systemctl poweroff
+  ;;
+$reboot)
+  systemctl reboot
+  ;;
+$lock)
+  i3lock
+  ;;
+$suspend)
+  systemctl suspend
+  ;;
+$logout)
+  i3-msg exit
+  ;;
+esac

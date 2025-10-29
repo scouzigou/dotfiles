d6a00f49ec50a8b7cd73e0efb646f49b70db04e3
scouzigou <noreply>
2025-10-29 23:07:20 +0100

    refactor: use tokyonight_night for neovim

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,49 +1,63 @@
 return {
+  -- tokyonight theme
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    "folke/tokyonight.nvim",
     lazy = true,
-    priority = 1000,
-    opts = function()
-      return {
-        integrations = {
-          aerial = true,
-          alpha = true,
-          cmp = true,
-          dashboard = true,
-          flash = true,
-          gitsigns = true,
-          headlines = true,
-          illuminate = true,
-          indent_blankline = { enabled = true },
-          leap = true,
-          lsp_trouble = true,
-          mason = true,
-          markdown = true,
-          mini = true,
-          native_lsp = {
-            enabled = true,
-            underlines = {
-              errors = { "undercurl" },
-              hints = { "undercurl" },
-              warnings = { "undercurl" },
-              information = { "undercurl" },
-            },
-          },
-          navic = { enabled = true, custom_bg = "lualine" },
-          neotest = true,
-          neotree = true,
-          noice = true,
-          notify = true,
-          semantic_tokens = true,
-          telescope = true,
-          treesitter = true,
-          treesitter_context = true,
-          which_key = true,
-        },
-      }
-    end,
+    opts = { style = "night" },
   },
+  -- catppuccin theme
+  -- {
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  --   lazy = true,
+  --   priority = 1000,
+  --   opts = function()
+  --     return {
+  --       integrations = {
+  --         aerial = true,
+  --         alpha = true,
+  --         cmp = true,
+  --         dashboard = true,
+  --         flash = true,
+  --         gitsigns = true,
+  --         headlines = true,
+  --         illuminate = true,
+  --         indent_blankline = { enabled = true },
+  --         leap = true,
+  --         lsp_trouble = true,
+  --         mason = true,
+  --         markdown = true,
+  --         mini = true,
+  --         native_lsp = {
+  --           enabled = true,
+  --           underlines = {
+  --             errors = { "undercurl" },
+  --             hints = { "undercurl" },
+  --             warnings = { "undercurl" },
+  --             information = { "undercurl" },
+  --           },
+  --         },
+  --         navic = { enabled = true, custom_bg = "lualine" },
+  --         neotest = true,
+  --         neotree = true,
+  --         noice = true,
+  --         notify = true,
+  --         semantic_tokens = true,
+  --         telescope = true,
+  --         treesitter = true,
+  --         treesitter_context = true,
+  --         which_key = true,
+  --       },
+  --     }
+  --   end,
+  -- },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "catppuccin-mocha",
+  --   },
+  -- },
+  -- solarized osaka theme
   -- {
   --   "craftzdog/solarized-osaka.nvim",
   --   lazy = true,
@@ -54,10 +68,4 @@ return {
   --     }
   --   end,
   -- },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin-mocha",
-    },
-  },
 }
